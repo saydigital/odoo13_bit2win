18e59fd2c52fa5f056781137494c7a3ccb6e76fe
Davide Fella <davide.fella@gmail.com>
2020-07-22 18:10:24 +0200

    Refactoring, bug fixing, test

--- sla_customer_pause/models/models.py
+++ sla_customer_pause/models/models.py
@@ -2,6 +2,8 @@
 
 from odoo import api, fields, models, _
 from odoo import exceptions 
+from datetime import datetime, timedelta
+
 
 class HelpdeskTicket(models.Model):
     _inherit = "helpdesk.ticket"
@@ -14,8 +16,7 @@ class HelpdeskTicket(models.Model):
         if self._is_awaiting_state(vals.get('stage_id')):
             vals['awaiting_start'] = fields.Datetime.now()
         elif self._is_prev_state_awaiting():
-            self._recompute_sla_after_waiting()
-            
+            self.recompute_sla_after_waiting()
             
             vals['awaiting_start'] = None
         
@@ -26,7 +27,7 @@ class HelpdeskTicket(models.Model):
         
         for status in self.sla_status_ids:
 
-            #If SLA closed break this iteration            
+            # If SLA closed break this iteration            
             if status.reached_datetime != False:
                 continue
             
@@ -35,37 +36,42 @@ class HelpdeskTicket(models.Model):
             if not working_calendar:
                 status.deadline = deadline
                 continue
-
+            
+            sla_time_days = status.sla_id.time_days + 1
+            elapsed_time_from_ticket_cration = self.compute_elapsed_time_from_ticket_creation(status.ticket_id.create_date, datetime_now)
+            elapsed_time_in_awaiting_state = datetime_now - status.ticket_id.awaiting_start
+            
             remainig_sla_days = self.compute_remainig_sla_days(
-                                        (status.sla_id.time_days + 1), 
-                                        self.compute_elapsed_time_from_ticket_cration(status.ticket_id.create_date, datetime_now), 
-                                        (datetime_now - status.ticket_id.awaiting_start ))            
+                                        sla_time_days,
+                                        elapsed_time_from_ticket_cration,
+                                        elapsed_time_in_awaiting_state)            
 
             if status.sla_id.time_days > 0:
                 status.sudo().deadline = self.compute_deadline(working_calendar, remainig_sla_days, datetime_now, datetime_now, status.sla_id.time_hours)
 
-    #To-Do: il calcolo dei giorni di SLA rimanenti deve tenere conto dei giorni festivi? 
-    def compute_remainig_sla_days(self, days_for_sla_completion, elapsed_time_from_ticket_cration, awaiting_time ):
-        return (days_for_sla_completion - elapsed_time_from_ticket_cration.days + awaiting_time.days)
+    def compute_remainig_sla_days(self, days_for_sla_completion, elapsed_time_from_ticket_cration, awaiting_time):
+        
+        return (timedelta(days=days_for_sla_completion) - elapsed_time_from_ticket_cration + awaiting_time).days
     
-    def compute_elapsed_time_from_ticket_cration(self, ticket_create_date, time_now):
+    def compute_elapsed_time_from_ticket_creation(self, ticket_create_date, time_now):
         if(time_now < ticket_create_date):
             raise exceptions.ValidationError('The ticket creation date cannot be after than now')
         
-        return time_now - ticket_create_date
+        work_days = self.team_id.resource_calendar_id.get_work_duration_data(ticket_create_date, time_now, compute_leaves=True)
+        
+        return timedelta(days=int(work_days['days']), seconds=int(work_days['hours'] * 60 * 60))
     
     def compute_deadline(self, working_calendar, remainig_sla_days, deadline, create_dt, time_hours):
         deadline = working_calendar.plan_days(remainig_sla_days, deadline, compute_leaves=True)
                 
         deadline.replace(hour=create_dt.hour, minute=create_dt.minute, second=create_dt.second, microsecond=create_dt.microsecond)
     
-        return self.compute_plan_hours(time_hours,deadline)
+        return self.compute_plan_hours(working_calendar, time_hours, deadline)
     
-    def compute_plan_hours(self, time_hours, deadline):
+    def compute_plan_hours(self, working_calendar, time_hours, deadline):
         return working_calendar.plan_hours(time_hours, deadline, compute_leaves=True)
-    
 
-    #Utilities
+    # Utilities
     def _is_prev_state_awaiting(self):
         return self._is_awaiting_state(self.stage_id.id)
     
@@ -77,4 +83,4 @@ class HelpdeskTicket(models.Model):
         if stage_id.id == False: 
             return False
         
-        return (self.AWAITING_STATE in stage_id.name.lower())
+        return (AWAITING_STATE in stage_id.name.lower())
--- sla_customer_pause/tests/test_sla_pause.py
+++ sla_customer_pause/tests/test_sla_pause.py
@@ -9,11 +9,18 @@ class TestHelpdeskTicket(TransactionCase):
     def setUp(self):
         super(TestHelpdeskTicket, self).setUp()
 
-        self.HelpdeskSlaTicket = self.env['helpdesk.ticket']
+        self.HelpdeskSlaTicket = self.env['helpdesk.ticket'].create({
+            'name':'Test SLA da codice'})
 
-        self.awaitingStage = self.env['helpdesk.stage'].create({'name':'Awaiting', 'sequence': 1, 'is_close': False, 'fold':True })
+        self.awaitingStage = self.env['helpdesk.stage'].create({
+            'name':'Awaiting', 
+            'sequence': 1, 
+            'is_close': False, 
+            'fold':True })
     
     
+    
+    #COMPUTE_REMAINING_SLA_DAYS
     def test_Sum_OneWeekAwaiting_compute(self):
         # arrange
         days_for_sla_completion = 14
@@ -27,14 +34,16 @@ class TestHelpdeskTicket(TransactionCase):
     
         # assert
         self.assertEqual(remaining_days,5)
-    
+
+
+    #COMPUTE_ELAPSED_TIME_FROM_TICKET_CREATION
     def test_Difference_SameMonthsDifferentDays_compute(self):
         # arrange
         time_end_awaiting = fields.Datetime.now().replace(day=20, month=7, year=2020, hour=12, minute=0, second=0)
         date_creation_ticket = fields.Datetime.now().replace(day=1, month=7, year=2020, hour=12, minute=0, second=0)
         
         # act
-        time_from_ticket_creation = self.HelpdeskSlaTicket.compute_elapsed_time_from_ticket_cration(date_creation_ticket, time_end_awaiting)
+        time_from_ticket_creation = self.HelpdeskSlaTicket.compute_elapsed_time_from_ticket_creation(date_creation_ticket, time_end_awaiting)
         
         # assert 
         self.assertEqual(time_from_ticket_creation.days, 19)
@@ -42,14 +51,14 @@ class TestHelpdeskTicket(TransactionCase):
         
     def test_Difference_MonthsYearDaysDifferent_compute(self):
         # arrange
-        time_end_awaiting = fields.Datetime.now().replace(day=21, month=7, year=2020, hour=17, minute=37, second=7)
-        date_creation_ticket = fields.Datetime.now().replace(day=1, month=6, year=2018, hour=20, minute=50, second=4)
+        date_creation_ticket = fields.Datetime.now().replace(day=1, month=7, year=2020, hour=20, minute=0, second=4)
+        time_end_awaiting = fields.Datetime.now().replace(day=31, month=7, year=2020, hour=20, minute=0, second=7)
         
         # act
-        time_from_ticket_creation = self.HelpdeskSlaTicket.compute_elapsed_time_from_ticket_cration(date_creation_ticket, time_end_awaiting)
+        time_from_ticket_creation = self.HelpdeskSlaTicket.compute_elapsed_time_from_ticket_creation(date_creation_ticket, time_end_awaiting)
         
         # assert 
-        self.assertEqual(time_from_ticket_creation.days, 780)
+        self.assertEqual(time_from_ticket_creation.days, 23)
         self.assertEqual(time_from_ticket_creation.seconds, 74823)
     
     def test_Difference_DaysTheSame_compute(self):
@@ -58,7 +67,7 @@ class TestHelpdeskTicket(TransactionCase):
         date_creation_ticket = fields.Datetime.now().replace(day=20, month=7, year=2020, hour=12, minute=0, second=0)
         
         # act
-        time_from_ticket_creation = self.HelpdeskSlaTicket.compute_elapsed_time_from_ticket_cration(date_creation_ticket, time_end_awaiting) 
+        time_from_ticket_creation = self.HelpdeskSlaTicket.compute_elapsed_time_from_ticket_creation(date_creation_ticket, time_end_awaiting) 
 
         # assert 
         self.assertEqual(time_from_ticket_creation.days, 0)
@@ -70,8 +79,33 @@ class TestHelpdeskTicket(TransactionCase):
         date_creation_ticket = fields.Datetime.now().replace(day=10, month=7, year=2020, hour=12, minute=0, second=0)
         
         # act
-        #self.assertRaises(self.HelpdeskSlaTicket.compute_elapsed_time_from_ticket_cration(time_end_awaiting, date_creation_ticket))
+        #self.assertRaises(self.HelpdeskSlaTicket.compute_elapsed_time_from_ticket_creation(time_end_awaiting, date_creation_ticket))
 
         # assert 
         #self.assertException(time_from_ticket_creation)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    def test_work_duration(self):
+        # arrange
+        time_end_awaiting = fields.Datetime.now().replace(day=26, month=12, year=2020, hour=12, minute=0, second=0)
+        date_creation_ticket = fields.Datetime.now().replace(day=25, month=12, year=2020, hour=12, minute=0, second=0)
+        
+        
+        calendar_id = self.env['resource.calendar'].browse(1)
+        
+        work_duration = calendar_id.get_work_duration_data(date_creation_ticket, time_end_awaiting, compute_leaves=True)
+        
+        a = 0
+        
+        #duration_data = status.ticket_id.team_id.resource_calendar_id.get_work_duration_data(start_dt, end_dt, compute_leaves=True)
+
                
