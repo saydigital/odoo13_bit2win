c6bffee38f322cd86c54ab92e183b7598d3eab73
Lucio <lucio.valente@infosons.com>
2020-09-27 20:58:54 +0200

    graphic fix

--- syd_custom/__manifest__.py
+++ syd_custom/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Custom B2W",
-    'version': '0.1.2',
+    'version': '0.1.3',
     'license': 'Other proprietary',
     'summary': """Helpdesk Extended
     """,
