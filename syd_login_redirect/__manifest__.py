5a7671308e8ad02119233e830ae14debf655ef05
Davide Fella <davide.fella@gmail.com>
2020-10-01 11:09:35 +0200

    New module that replace the previous login module

---
+++ syd_login_redirect/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "syd_login_redirect",
+    'version': '0.2',
+    'author': "Rapsodoo",
+    'maintainer': 'Rapsodoo',
+    'website': "http://www.yourcompany.com",
+    'category': 'Base',
+    'depends': ['base','website'],
+    'installable': True,
+    'license': 'AGPL-3',    
+    'data': [
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+}
---
+++ syd_login_redirect/controllers/controllers.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from odoo import http
+from odoo.http import request
+from odoo.addons.web.controllers.main import WebClient, Home
+from odoo.addons.website.controllers.main import Website
+from odoo.addons.website_form.controllers.main import WebsiteForm
+from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager, get_records_pager
+
+
+class Website(Home):
+    
+    @http.route('/', type='http', auth="public", website=True)
+    def index(self, **kw):
+        
+        # TODO: Check if user is Portal!!
+        if(request.session.uid != None):  # User Auth
+            homepage = request.website.menu_id.url_logged 
+            
+            if homepage and (request.env.user.has_group('base.group_user') or request.env.user.has_group('base.group_portal')) and homepage != '/':
+                return request.env['ir.http'].reroute(homepage)
+        # End custom fix 
+        
+        return super(Website, self).index(**kw)
