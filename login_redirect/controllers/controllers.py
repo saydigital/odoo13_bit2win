71f927a292adf59e6b85d82177a13b8c8c9f198e
Lucio <lucio.valente@infosons.com>
2020-10-01 08:51:06 +0200

    refactoring

--- login_redirect/controllers/controllers.py
+++ login_redirect/controllers/controllers.py
@@ -17,21 +17,8 @@ class Website(Home):
         if(request.session.uid != None):  # User Auth
             homepage = request.website.menu_id.url_logged 
             
-            if homepage and request.env.user.has_group('base.group_user') and homepage != '/':
+            if homepage and request.env.user.has_group('base.portal_user') and homepage != '/':
                 return request.env['ir.http'].reroute(homepage)
         # End custom fix 
         
-        homepage = request.website.homepage_id
-        if homepage and (homepage.sudo().is_visible or request.env.user.has_group('base.group_user')) and homepage.url != '/':
-            return request.env['ir.http'].reroute(homepage.url)
-
-        website_page = request.env['ir.http']._serve_page()
-        if website_page:
-            return website_page
-        else:
-            top_menu = request.website.menu_id
-            first_menu = top_menu and top_menu.child_id and top_menu.child_id.filtered(lambda menu: menu.is_visible)
-            if first_menu and first_menu[0].url not in ('/', '', '#') and (not (first_menu[0].url.startswith(('/?', '/#', ' ')))):
-                return request.redirect(first_menu[0].url)
-
-        raise request.not_found()
+        return super(Website,self).index(**kw)
