d5cb021b8cf6cbb3396b1878b6c8989090228697
Lucio <lucio.valente@infosons.com>
2020-10-06 13:00:18 +0200

    customer mail

--- syd_customer_mail_internal/models/mail_message.py
+++ syd_customer_mail_internal/models/mail_message.py
@@ -4,7 +4,7 @@ from odoo import _, api, fields, models, modules, tools
 
 class Message(models.Model):
     _inherit = 'mail.message'
- 
+    
     @api.model
     def _get_default_from(self):
         if self.env.user.email:
@@ -12,3 +12,9 @@ class Message(models.Model):
                 return tools.formataddr((self.env.user.name, self.env.user.partner_ids[0].email_internal))
             return tools.formataddr((self.env.user.name, self.env.user.email))
         raise UserError(_("Unable to post message, please configure the sender's email address."))
+
+    email_from = fields.Char(
+        'From', default=_get_default_from,
+        help="Email address of the sender. This field is set when no matching partner is found and replaces the author_id field in the chatter.")
+    
+    
