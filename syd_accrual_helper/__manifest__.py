22c6a5dff3341748da68f471d203cfd56ba54bfa
Lucio <lucio.valente@infosons.com>
2020-10-25 22:56:14 +0100

    accruals

---
+++ syd_accrual_helper/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': "Accrual Helper",
+    'version': '0.0.1',
+    'license': 'Other proprietary',
+    'summary': """Accrual Helper
+    """,
+    'author': "Rapsodoo",
+    'website': "http://www.rapsodoo.com",
+    'category' : 'Account',
+    'depends': [
+                'account',
+                
+                ],
+    'data':[
+       'wizard/wizard.xml',
+        'views/views.xml'
+        
+    ],
+    'installable' : True,
+    'application' : False,
+}
+
