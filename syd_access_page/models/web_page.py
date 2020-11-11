0f0adf72669b4dda82c48b3185cdf2c30f865910
Davide Fella <davide.fella@gmail.com>
2020-11-11 15:27:00 +0100

    Access module page

---
+++ syd_access_page/models/web_page.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _, SUPERUSER_ID
+from odoo.http import request
+import werkzeug.utils
+
+class Website(models.Model):
+    _inherit = "website"
+    
+    url_authenticated = fields.Char('Page not auth')
+
+class Page(models.Model):
+    _inherit = "website.page"
+    
+    authenticated_page = fields.Boolean('Authentication Page')
+
+class Http(models.AbstractModel):
+    _inherit = 'ir.http'
+    
+    @classmethod
+    def _serve_page(cls):
+        page = super(Http, cls)._serve_page()
+            
+        page_domain = [('url', '=', request.httprequest.path)] + request.website.website_domain()
+        page_searched = request.env['website.page'].sudo().search(page_domain, order='website_id asc', limit=1)
+        
+        # If page not found or user is authenticated, follow the standard flow 
+        if(page == False or request.session.uid != None 
+             or request.httprequest.path == request.website.url_authenticated
+             or request.httprequest.path == "/" 
+             or page_searched.authenticated_page == False):
+            return page
+        
+        return werkzeug.utils.redirect(request.website.url_authenticated)
