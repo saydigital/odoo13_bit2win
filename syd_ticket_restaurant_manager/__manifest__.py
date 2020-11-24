b4533571d8c47d3b76150f2f54a90e37beddca9c
Davide Fella <davide.fella@gmail.com>
2020-11-25 00:22:16 +0100

    Module init

---
+++ syd_ticket_restaurant_manager/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "syd_ticket_restaurant_manager",
+
+    'description': """
+       Ticket restaurant manager ticket
+    """,
+
+    'author': "Rapsodoo",
+
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ]
+}
