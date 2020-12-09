0a548118f565f9980141102579a3ba6847a77f75
Davide Fella <davide.fella@gmail.com>
2020-12-09 18:05:29 +0100

    new fields

--- syd_bank_accounting_report/__manifest__.py
+++ syd_bank_accounting_report/__manifest__.py
@@ -9,7 +9,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '6.0',
+    'version': '7.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
