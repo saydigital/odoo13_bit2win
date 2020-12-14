8ae83429382c0337fcb8ddd19d86918e048afb42
Davide Fella <davide.fella@gmail.com>
2020-12-14 20:06:01 +0100

    Fist module version

---
+++ syd_check_businessmail/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "syd_check_businessmail",
+
+    'author': "Rapsodoo",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'crm'],
+
+    # always loaded
+    'data': [
+        'views/views.xml',
+        'views/templates.xml',
+    ]
+}
