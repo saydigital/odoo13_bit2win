e25eec37a308eca21abb9ad73863000134d5492d
Davide Fella <davide.fella@gmail.com>
2020-12-17 18:23:54 +0100

    Bug fixing

--- syd_check_businessmail/__manifest__.py
+++ syd_check_businessmail/__manifest__.py
@@ -5,7 +5,7 @@
     'author': "Rapsodoo",
 
     'category': 'Uncategorized',
-    'version': '3.0',
+    'version': '3.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'sales_team', 'crm'],
