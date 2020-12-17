426ed81e932b5e5d8711943e9ad7887dc1a8eb5a
Davide Fella <davide.fella@gmail.com>
2020-12-17 13:05:07 +0100

    Version update

--- syd_check_businessmail/__manifest__.py
+++ syd_check_businessmail/__manifest__.py
@@ -5,7 +5,7 @@
     'author': "Rapsodoo",
 
     'category': 'Uncategorized',
-    'version': '1.1',
+    'version': '2.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'sales_team', 'crm'],
