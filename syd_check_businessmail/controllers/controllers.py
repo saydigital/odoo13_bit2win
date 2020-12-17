289825e76baeb9279ffabcf4e2ff39ae2fa77eb6
Davide Fella <davide.fella@gmail.com>
2020-12-17 13:04:06 +0100

    Add new features

--- syd_check_businessmail/controllers/controllers.py
+++ syd_check_businessmail/controllers/controllers.py
@@ -5,55 +5,60 @@ from odoo import http
 from odoo.http import request
 from odoo.addons.website_form.controllers.main import WebsiteForm
 from odoo.exceptions import UserError, ValidationError
+from odoo.modules.module import get_module_resource
 
+import inspect
+import re
+import os
+
+#This email control class is also in the controller with some small difference. 
+#TO-DO: Refactor code for a unique class
 class WebsiteForm(WebsiteForm):
-    _name = "website"
-    _inherit = ["syd_check_businessmail.business_mail_validator_mixin"]
+    #_inherit = ["syd_check_businessmail.business_mail_validator_mixin"]
 
     def extract_data(self, model, values):
         
         self._check_mails(values)
         
-        super(WebsiteForm, self).extract_data(model, values)
+        return super(WebsiteForm, self).extract_data(model, values)
         
         
     def _check_mails(self, vals):
-        #Have to activate the from the sales team page    
-        #if(self.env['crm.team'].search([('id','=', vals['team_id'])]).mail_control_active == False): 
-        #    return True
+        #Have to activate the from the sales team page 
+        if(len(request.env['crm.team']) == 0):
+            return True
+           
+        if(request.env['crm.team'].search([('id','=', vals['team_id'])]).mail_control_active == False): 
+            return True
         
         #if(request.env['crm.team'].search([('id','=', request.params['team_id'])]).mail_control_active)
         
         if ('email_from' in vals):
             if(vals['email_from'] != False):
                 if(self._check_valid_mail(vals['email_from']) == False): 
-                    raise UserError("Plese, insert a corporate mail address") 
+                    raise ValidationError({"email_from": "Please, insert a valid corporate mail address"}) 
 
                 if(self._is_business_mail(vals['email_from']) == False): 
                     self.env["refused_email"].sudo().create({"domain_failed": self._get_mail_address_domain(vals['email_from'])})
                     self._cr.commit()
-
-                    raise UserError("Plese, insert a corporate mail address") 
+                    raise ValidationError({"email_from": "Please, insert a valid corporate mail address"}) 
         
         if ('email_cc' in vals):
             if(vals['email_cc'] != False):
                 if(self._check_valid_mail(vals['email_cc']) == False): 
-                    raise UserError("Please, insert a corporate mail cc address")   
+                    raise ValidationError({"email_from": "Please, insert a valid corporate mail address in cc"}) 
         
                 if(self._is_business_mail(vals['email_cc']) == False): 
                     self.env["refused_email"].sudo().create({"domain_failed": self._get_mail_address_domain(vals['email_cc'])})
                     self._cr.commit()
                     
-                    raise UserError("Plese, insert a corporate mail in cc field")     
+                    raise ValidationError({"email_from": "Please, insert a valid corporate mail address in cc"}) 
         
-        self.env["refused_email"].write({"domain_failed": "testdavide"})
-
     def _is_business_mail(self, email_address):
   
-        directory_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))        
-        complete_file_path = os.path.join(directory_path, 'blacklist_domains.txt')
-        
-        with open(complete_file_path) as file:
+        file_path = get_module_resource('syd_check_businessmail', 'static', 'text', 'blacklist_domains.txt')
+
+        with open(file_path) as file:
             black_list_domains = [line.rstrip() for line in file]        
         
         if(self._get_mail_address_domain(email_address) in black_list_domains ):
--- syd_check_businessmail/models/crm_models.py
+++ syd_check_businessmail/models/crm_models.py
@@ -10,47 +10,65 @@ import os
 from odoo import http
 from odoo.http import request
 from odoo.addons.website_form.controllers.main import WebsiteForm
+from odoo.modules.module import get_module_resource
+
+#This email control class is also in the controller with some small difference. 
+#TO-DO: Refactor code for a unique class
+#class BusinessMailValidationMixin(models.AbstractModel):
+#    _name = "syd_check_businessmail.business_mail_validator_mixin"
+#    _description = 'Mixing for validate and check if email address is a business one'
+    
+    
+class CrmTeam(models.Model):
+    _inherit = "crm.team"
+    
+    mail_control_active = fields.Boolean('Email control active')
 
 
-class BusinessMailValidationMixin(models.AbstractModel):
-    _name = "syd_check_businessmail.business_mail_validator_mixin"
-    _description = 'Mixing for validate and check if email address is a business one'
+class Lead(models.Model):
+    _name = "crm.lead"
+    _inherit = ["crm.lead"]
+
+    @api.model
+    def create(self, vals):
+        
+        if(request.is_frontend == False):
+            self._check_mails(vals)
+        
+        return super(Lead, self).create(vals)
     
     def _check_mails(self, vals):
-        #Have to activate the from the sales team page    
+        
         if(self.env['crm.team'].search([('id','=', vals['team_id'])]).mail_control_active == False): 
             return True
         
         if ('email_from' in vals):
             if(vals['email_from'] != False):
                 if(self._check_valid_mail(vals['email_from']) == False): 
-                    raise UserError("Plese, insert a corporate mail address") 
+                    raise ValidationError("Please, insert a valid corporate mail address") 
 
                 if(self._is_business_mail(vals['email_from']) == False): 
                     self.env["refused_email"].sudo().create({"domain_failed": self._get_mail_address_domain(vals['email_from'])})
                     self._cr.commit()
 
-                    raise UserError("Plese, insert a corporate mail address") 
+                    raise ValidationError("Please, insert a valid corporate mail address") 
         
         if ('email_cc' in vals):
             if(vals['email_cc'] != False):
                 if(self._check_valid_mail(vals['email_cc']) == False): 
-                    raise UserError("Please, insert a corporate mail cc address")   
+                    raise ValidationError("Please, insert a valid corporate mail address") 
         
                 if(self._is_business_mail(vals['email_cc']) == False): 
                     self.env["refused_email"].sudo().create({"domain_failed": self._get_mail_address_domain(vals['email_cc'])})
                     self._cr.commit()
                     
-                    raise UserError("Plese, insert a corporate mail in cc field")     
+                    raise ValidationError("Please, insert a valid corporate mail address") 
         
-        self.env["refused_email"].write({"domain_failed": "testdavide"})
-
     def _is_business_mail(self, email_address):
   
-        directory_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))        
-        complete_file_path = os.path.join(directory_path, 'blacklist_domains.txt')
+        file_path = get_module_resource('syd_check_businessmail', 'static', 'text', 'blacklist_domains.txt')
         
-        with open(complete_file_path) as file:
+        with open(file_path) as file:
             black_list_domains = [line.rstrip() for line in file]        
         
         if(self._get_mail_address_domain(email_address) in black_list_domains ):
@@ -66,21 +84,3 @@ class BusinessMailValidationMixin(models.AbstractModel):
         
     def _get_mail_address_domain(self, email_address):
         return email_address.split('@')[1]
-
-class CrmTeam(models.Model):
-    _inherit = "crm.team"
-    
-    mail_control_active = fields.Boolean('Email control active')
-
-
-class Lead(models.Model):
-    _name = "crm.lead"
-    _inherit = ["crm.lead", "syd_check_businessmail.business_mail_validator_mixin"]
-
-    @api.model
-    def create(self, vals):
-        
-        #check_mails return True if mails are valid but throw and exception if they don't
-        self._check_mails(vals)
-        
-        return super(Lead, self).create(vals)
