98d33a79d86745f632e90e2b1ab9eace705931c0
Davide Fella <davide.fella@gmail.com>
2021-03-12 12:20:55 +0100

    Code refactoring

--- syd_custom/__manifest__.py
+++ syd_custom/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Custom B2W",
-    'version': '0.1.23',
+    'version': '0.1.24',
     'license': 'Other proprietary',
     'summary': """Helpdesk Extended
     """,
